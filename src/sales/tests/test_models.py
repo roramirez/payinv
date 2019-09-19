8eb46f11c855f8912d6d6f843b3a66cc4c99c522
Rodrigo Ramírez Norambuena <a@rodrigoramirez.com>
2019-09-19 18:16:12 -0400

    add test for Sale model

---
+++ src/sales/tests/test_models.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+from core.tests.factories import SaleFactory, PaymentFactory, InvoiceFactory
+
+
+class SaleModelTestCase(TestCase):
+
+    def test_total_payments_in_cero(self):
+        """ Should return cero for sales without payments"""
+        sale = SaleFactory(total_value=100)
+        self.assertEqual(sale.total_payments, 0)
+
+    def test_total_invoices_in_cero(self):
+        """ Should return cero for sales without invoices"""
+        sale = SaleFactory(total_value=100)
+        self.assertEqual(sale.total_invoices, 0)
+
+    def test_total_payments(self):
+        """ Should return total value for payments of sale"""
+        sale = SaleFactory(total_value=1000)
+        PaymentFactory(sale=sale, total_value=50)
+        PaymentFactory(sale=sale, total_value=50)
+        self.assertEqual(sale.total_payments, 100)
+
+    def test_total_invoices(self):
+        """ Should return total value for invoices of sale"""
+        sale = SaleFactory(total_value=1000)
+        InvoiceFactory(sale=sale, total_value=50)
+        InvoiceFactory(sale=sale, total_value=500)
+        self.assertEqual(sale.total_invoices, 550)
