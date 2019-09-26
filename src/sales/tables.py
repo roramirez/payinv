a6555bc71313621248c7727880ac97c1e8affc47
Rodrigo Ramírez Norambuena <a@rodrigoramirez.com>
2019-09-26 11:02:03 -0400

    Add url link in sales list for Invoices and Payments

--- src/sales/tables.py
+++ src/sales/tables.py
@@ -3,6 +3,8 @@ from django_tables2.utils import Accessor
 from utilities.tables import BaseTable
 from sales.models import Sale
 from django.utils.translation import ugettext as _
+from django.utils.html import format_html
+from django.urls import reverse
 
 
 class SaleTable(BaseTable):
@@ -17,12 +19,22 @@ class SaleTable(BaseTable):
     done_at = tables.Column(verbose_name=_('Date'))
 
     # FIXME: Make me happy. Sort this colums
-    total_payments = tables.Column(verbose_name=_('Total Payments'),
-                                   orderable=False)
-    total_invoices = tables.Column(verbose_name=_('Total Invoices'),
-                                   orderable=False)
+    total_payments = tables.LinkColumn(verbose_name=_('Total Payments'),
+                                       orderable=False)
+    total_invoices = tables.LinkColumn(verbose_name=_('Total Invoices'),
+                                       orderable=False)
 
     class Meta(BaseTable.Meta):
         model = Sale
         fields = ('internal_id', 'customer',
                   'concept', 'total_value', 'done_at')
+
+    def render_total_invoices(self, record):
+        url = reverse('invoice_list')
+        return format_html('<a href="{}?sale={}">{}</a>',
+                           url, record.id, record.total_invoices)
+
+    def render_total_payments(self, record):
+        url = reverse('payment_list')
+        return format_html('<a href="{}?sale={}">{}</a>',
+                           url, record.id, record.total_payments)
