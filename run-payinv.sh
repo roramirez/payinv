58a40c6948ee22eeed30cc2406b7d1dcb11d6eca
Brian L <bluna9995@gmail.com>
2019-10-02 22:12:45 -0300

    only compile for Spanish(ES) messages
    Signed-off-by: Brian L <bluna9995@gmail.com>

--- run-payinv.sh
+++ run-payinv.sh
@@ -3,6 +3,6 @@
 python src/manage.py migrate
 python src/manage.py check --deploy
 python src/manage.py collectstatic --no-input
-python src/manage.py compilemessages
+python src/manage.py compilemessages -l es
 
 python src/manage.py runserver
