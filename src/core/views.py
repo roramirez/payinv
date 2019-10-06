8ef7ab0f9b35b75ddfd75611d8965657c8ce397c
Rodrigo Ramírez Norambuena <a@rodrigoramirez.com>
2019-10-06 15:40:13 -0400

    Add summary total values for Invoices and Payments pending

--- src/core/views.py
+++ src/core/views.py
@@ -3,14 +3,21 @@ import customers
 import sales
 import payments
 import invoices
-from django.db.models import Count
+from django.db.models import Count, Sum
 
 
 def HomePage(request):
     """ home page """
 
     invoices_pending = invoices.models.Invoice.sales_pending()
+    invoices_pending_total = invoices_pending.aggregate(
+            Sum('total_value'))['total_value__sum'] or 0
+
     payments_pending = payments.models.Payment.sales_pending()
+    payments_pending_total = payments_pending.aggregate(
+            Sum('total_value'))['total_value__sum'] or 0
+
+
 
     invoices_pending_by_customer = invoices_pending.values(
             'customer__name', 'customer').annotate(
@@ -24,8 +31,10 @@ def HomePage(request):
     dashboard = {
         'customer': customers.models.Customer.objects.all(),
         'invoices_pending': invoices_pending,
+        'invoices_pending_total': invoices_pending_total,
         'invoices_pending_by_customer': invoices_pending_by_customer,
         'payments_pending': payments_pending,
+        'payments_pending_total': payments_pending_total,
         'payments_pending_by_customer': payments_pending_by_customer,
         'table_pending': sales.tables.SaleTable,
 
