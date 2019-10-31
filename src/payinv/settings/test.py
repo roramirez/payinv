e259546b79780d15b1cc140bf13a341150dd4407
Rodrigo Ramírez Norambuena <a@rodrigoramirez.com>
2019-10-31 11:53:17 -0400

    Add  nplusone to find N+1 Queries

--- src/payinv/settings/test.py
+++ src/payinv/settings/test.py
@@ -1,3 +1,28 @@
 from .base import *
+import logging
 
 DEBUG = True
+
+
+INSTALLED_APPS += ('nplusone.ext.django', )
+
+
+MIDDLEWARE.append('nplusone.ext.django.NPlusOneMiddleware')
+
+NPLUSONE_LOGGER = logging.getLogger('nplusone')
+NPLUSONE_LOG_LEVEL = logging.WARN
+
+LOGGING = {
+    'version': 1,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            },
+    },
+    'loggers': {
+        'nplusone': {
+            'handlers': ['console'],
+            'level': 'WARN',
+            },
+    },
+}
