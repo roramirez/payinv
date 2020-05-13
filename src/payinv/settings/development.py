b74eaac10bb2c31676feecf17871929aa6bc58a2
Rodrigo Ramírez Norambuena <a@rodrigoramirez.com>
2020-05-13 13:44:58 -0400

    enable db backend queries in dev enviroment

--- src/payinv/settings/development.py
+++ src/payinv/settings/development.py
@@ -4,3 +4,21 @@ DEBUG = True
 TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = ['*']
+
+
+LOGGING = {
+    'version': 1,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'DEBUG',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+    }
+}
